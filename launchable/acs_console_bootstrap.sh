fe379def0f35f8b434343990712700fcf078d8dc
Kyle Tretina <ktretina@nvidia.com>
2026-08-13 11:16:21 -0400

    Repin ACS bootstrap to sequential lesson fix

--- launchable/acs_console_bootstrap.sh
+++ launchable/acs_console_bootstrap.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 umask 077
 
 readonly repo_url="https://github.com/ktretina/nvmolkit-brev-notebook.git"
-readonly repo_commit="a182a42dcf320ed5267c714ad8cb50509cf9e7fd"
+readonly repo_commit="a91cc22505c8965e689980d0f582181ede533846"
 readonly source_root="${HOME}/.local/share/acs-nemoclaw-launchable"
 readonly checkout_dir="${source_root}/source-${repo_commit}"
 
--- tests/test_acs_console_bootstrap.py
+++ tests/test_acs_console_bootstrap.py
@@ -13,7 +13,7 @@ AUTHORING_SHEET = ROOT / "launchable" / "ACS_LAUNCHABLE_FIELDS.md"
 PLACEHOLDER = "@REVIEWED_PUBLIC_COMMIT_SHA@"
 REPO_URL = "https://github.com/ktretina/nvmolkit-brev-notebook.git"
 DUMMY_COMMIT = "a" * 40
-PINNED_SETUP_COMMIT = "a182a42dcf320ed5267c714ad8cb50509cf9e7fd"
+PINNED_SETUP_COMMIT = "a91cc22505c8965e689980d0f582181ede533846"
 
 
 def _rendered_bootstrap() -> str:
